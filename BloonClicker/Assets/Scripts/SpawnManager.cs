4515e7ef833525420c9ebcb1fe272d60ce14dcf9
BobsLands <austin.d959@stu.nebo.edu>
2022-09-13 14:19:22 -0600

    bloona spawner

---
+++ BloonClicker/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] bloonaPrefabs;
+    public float startDelay = 0.5f;
+    public float spawnInterval = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBloona", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void SpawnRandomBloona()
+    {
+        // gets random position on axis
+        Vector3 spawnPos = new Vector3(Random.Range(-5,5),10,0);
+        // picks bloona off array
+        int bloonaIndex = Random.Range(0,bloonaPrefabs.Length);
+        //spawns bloona at spawn position
+
+        Instantiate(bloonaPrefabs[bloonaIndex], spawnPos, bloonaPrefabs[bloonaIndex].transform.rotation);
+
+    }
+}
